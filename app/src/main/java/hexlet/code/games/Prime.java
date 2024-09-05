0fb14b222fe54fe5045c3749ff195ed79fad1427
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-05 21:16:01 +0700

    Corrected Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,52 +4,38 @@ import hexlet.code.Engine; //Импортируем "Движок" Engine
 import java.util.Random; //Импортируем Класс Random
 
 public class Prime {
-    public static void prime() {
-        var random = new Random();  //Создали объект Random
+    public static final int NUMBER_START = 1;
+    public static final int NUMBER_LIMIT = 100;
 
-        System.out.println(Engine.userName()); //Узнать имя игрока
-        String name = Engine.userEntersString(); //Игрок вводит имя и для завершения нажимает Enter
-        System.out.println(Engine.helloUserName(name)); // Приветсвие игрока
+    public static void run() {
+        var questions = new String[Engine.ROUNDS_COUNT][];
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            questions[i] = generateRound();
+        }
+        var rulesGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        Engine.run(questions, rulesGame);
+    }
 
-        // Начало игры Prime
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+    public static String[] generateRound() {
+        var number = new Random().nextInt(NUMBER_START, NUMBER_LIMIT);
+        var correctAnswer = isPrime(number) ? "yes" : "no";
 
-        boolean flag = true;
-        for(int i = 0; i < 3; i++) {
-            var number = random.nextInt(100); //Любое число от 0 до 100
-            String answerYesOrNo = new String("yes"); //
+        return new String[]{String.valueOf(number), correctAnswer};
+    }
 
-            //Если число равно 2 или 3 то нет смычла
-            if (number == 2 || number == 3) {
-                answerYesOrNo = "yes";
-            } else if (number <= 1) {
-                answerYesOrNo = "no";
-            } else {
-                int halfNumber = number / 2;
-                for (int j = 2; j < number; j++) {
-                    if (number % j == 0) {
-                        answerYesOrNo = "no";
-                        break;
-                    }
-                }
-            }
-            System.out.println(Engine.questionTask(String.valueOf(String.valueOf(number)))); //Задается вопрос
-            var userAnswer = Engine.userEntersString(); //Игрок вводит свой ответ
-            System.out.println(Engine.yourAnswer(String.valueOf(userAnswer))); //Выводится ответ игрока
+    public static boolean isPrime(int number) {
+        boolean numberPrime = true;
 
-            if (answerYesOrNo.equals(userAnswer)) {
-                System.out.println(Engine.correct()); //Подтверждается правильность ответа
-            } else {
-                //Выводится если ответ не верный
-                System.out.println(Engine.stringwrongAnswer(name, userAnswer, answerYesOrNo));
-                flag = false; //И переменная flag меняет свое значение на false
+        if (number == 2 || number == 3) {
+            numberPrime = true;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                numberPrime = false;
                 break;
             }
-
         }
-        if (flag) {
-            //Если игрок три раза ответил правильно то его хвалят
-            System.out.println(Engine.congratulations(name));
-        }
+        return numberPrime;
     }
 }
