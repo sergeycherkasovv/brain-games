76aeff14627f5a1fbe3f5de25fc51d5bfc667cba
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-05 20:18:35 +0700

    Corrected Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,60 +3,55 @@ package hexlet.code.games;
 import hexlet.code.Engine; //Импортируем "Движок" Engine
 import java.util.Random; //Импортируем Класс Random
 
+
 public class Calc {
-    public static void calculated () {
-        var random = new Random();  //Создали объект Random
+    public static final int NUMBER_START = 1;
+    public static final int NUMBER_LIMIT = 100;
+    public static final int NUMBER_OPERATOR = 3;
 
-        System.out.println(Engine.userName()); //Узнать имя игрока
-        String name = Engine.userEntersString(); //Игрок вводит имя и для завершения нажимает Enter
-        System.out.println(Engine.helloUserName(name)); // Приветсвие игрока
-
-
-        // Начало игры Calc
-        System.out.println("What is the result of the expression?");
-
-        boolean flag = true;
-        for (int i = 0; i < 3; i++) {
-
-            var number1 = random.nextInt(100); //Первое число
-            var number2 = random.nextInt(100); //Второе число
-            var operatorNum = random.nextInt(3); //Выполает число в диапозоне от 0 до 3
-                                                        //для того чтобы выбрать один из операторов +, -, *
-            int sumInt = 0; // Сумма рандомных чисел
-            String sumString = ""; // Строковый вид примера
-
-            switch (operatorNum) {
-                case 0: //operator "+"
-                    sumInt = number1 + number2;
-                    sumString = number1 + " + " + number2;
-                    break;
-                case 1: //operator "-"
-                    sumInt = number1 - number2;
-                    sumString = number1 + " - " + number2;
-                    break;
-                case 2: //operator "*"
-                    sumInt = number1 * number2;
-                    sumString = number1 + " * " + number2;
-                    break;
-            }
-
-            System.out.println(Engine.questionTask(sumString)); //Задается вопрос
-            int userAnswer = Engine.userEntersInt(); //Игрок вводит свой ответ
-            System.out.println(Engine.yourAnswer(String.valueOf(userAnswer))); //Выводится ответ игрока
-
-            if (sumInt == userAnswer) {
-                System.out.println(Engine.correct()); //Подтверждается правильность ответа
-            } else {
-                //Выводится если ответ не верный
-                System.out.println(Engine.stringwrongAnswer(name, String.valueOf(userAnswer), String.valueOf(sumInt)));
-                flag = false; //И переменная flag меняет свое значение на false
-                break;
-            }
-
-            }
-        if (flag) {
-            //Если игрок три раза ответил правильно то его хвалят
-            System.out.println(Engine.congratulations(name));
+    public static void run() {
+        var questions = new String[Engine.ROUNDS_COUNT][];
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            questions[i] = generateRound();
         }
+        var rulesGame = "What is the result of the expression?";
+        Engine.run(questions, rulesGame);
+    }
+
+    public static String[] generateRound() {
+        var firstNumber = new Random().nextInt(NUMBER_START, NUMBER_LIMIT); //Первое число
+        var secondNumber = new Random().nextInt(NUMBER_START, NUMBER_LIMIT); //Второе число
+        var operator = new Random().nextInt(NUMBER_OPERATOR);//Выполяет число в диапозоне от 0 до 3
+                                                    //для того чтобы выбрать один из операторов +, -, *
+        char symbol;
+        switch (operator) {
+            case 0 -> symbol = '+';
+            case 1 -> symbol = '-';
+            case 2 -> symbol = '*';
+            default -> throw new RuntimeException("unknown number");
+        };
+
+        var calculatedAnswer = calculated(firstNumber, secondNumber,symbol);
+        var calculatedQuestions = calculatedString(firstNumber, secondNumber, symbol);
+
+        return new String[]{calculatedQuestions, calculatedAnswer};
+    }
+
+    public static String calculated(int firstNumber, int secondNumber, char symbol) {
+        return switch (symbol) {
+            case '+' -> String.valueOf(firstNumber + secondNumber);
+            case '-' -> String.valueOf(firstNumber - secondNumber);
+            case '*' -> String.valueOf(firstNumber * secondNumber);
+            default -> throw new RuntimeException("unknown operator");
+        };
+    }
+
+    public static String calculatedString(int firstNumber, int secondNumber, char symbol) {
+        return switch (symbol) {
+            case '+' -> firstNumber + " + " + secondNumber;
+            case '-' -> firstNumber + " - " + secondNumber;
+            case '*' -> firstNumber + " * " + secondNumber;
+            default -> throw new RuntimeException("unknown operator");
+        };
     }
 }
