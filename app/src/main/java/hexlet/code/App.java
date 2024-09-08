15c8e454e90881a25024e310d7dc3f2d45b7d67a
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-08 15:40:52 +0700

    corrected import in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
-import hexlet.code.games.*;
 
 import java.util.Scanner;
+
+import hexlet.code.games.Cli;
+import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
+
+
 
 public class App {
     public static final int GREAT = 1;
