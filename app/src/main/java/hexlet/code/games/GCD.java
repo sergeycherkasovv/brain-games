07a36a18762ead00b63a9ff1aa77444563d73153
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-08 17:06:58 +0700

    creating a constant with the rules of the game in GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -6,14 +6,15 @@ import hexlet.code.Utils;
 public class GCD {
     public static final int START_RANDOM = 1;
     public static final int LIMIT_RANDOM = 100;
+    public static final String RULES_GAME = "Find the greatest common divisor of given numbers.";
 
     public static void run() {
         var questions = new String[Engine.ROUNDS_COUNT][];
         for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
             questions[i] = generateRound();
         }
-        var rulesGame = "Find the greatest common divisor of given numbers.";
-        Engine.run(questions, rulesGame);
+
+        Engine.run(questions, RULES_GAME);
     }
 
     public static String[] generateRound() {
