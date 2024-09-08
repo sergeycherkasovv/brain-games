9d8d86b6fd4d23f04aff8e689c7292fa46866dfb
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-08 16:54:54 +0700

    corrected Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -7,7 +7,9 @@ import hexlet.code.Utils;
 public class Calc {
     public static final int START_RANDOM = 1;
     public static final int LIMIT_RANDOM = 100;
-    public static final int OPERATOR = 3;
+    public static final int OPERATOR_SYMBOL = 3;
+
+    public static final String[] SYMBOL = new String[]{"+", "-", "*"};
 
     public static void run() {
         var questions = new String[Engine.ROUNDS_COUNT][];
@@ -21,37 +23,25 @@ public class Calc {
     public static String[] generateRound() {
         var firstNumber = Utils.generateRandomNumber(START_RANDOM, LIMIT_RANDOM); //Первое число
         var secondNumber = Utils.generateRandomNumber(START_RANDOM, LIMIT_RANDOM); //Второе число
-        var operator = Utils.generateRandomNumber(OPERATOR); //Выполяет число в диапозоне от 0 до 3
+        var operator = Utils.generateRandomNumber(OPERATOR_SYMBOL); //Выполяет число в диапозоне от 0 до 3
                                                     //для того чтобы выбрать один из операторов +, -, *
-        char symbol;
-        symbol = switch (operator) {
-            case 0 -> '+';
-            case 1 -> '-';
-            case 2 -> '*';
-            default -> throw new RuntimeException("Unknown number");
-        };
 
-        var calculatedAnswer = calculatedInt(firstNumber, secondNumber, symbol);
-        var calculatedQuestions = calculatedString(firstNumber, secondNumber, symbol);
+        var calculatedQuestions = questionsCalculated(firstNumber, secondNumber, SYMBOL[operator]);
+        var calculatedAnswer = calculated(firstNumber, secondNumber, SYMBOL[operator]);
 
         return new String[]{calculatedQuestions, String.valueOf(calculatedAnswer)};
     }
 
-    public static int calculatedInt(int firstNumber, int secondNumber, char symbol) {
+    public static int calculated(int firstNumber, int secondNumber, String symbol) {
         return switch (symbol) {
-            case '+' -> firstNumber + secondNumber;
-            case '-' -> firstNumber - secondNumber;
-            case '*' -> firstNumber * secondNumber;
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
             default -> throw new RuntimeException("Unknown operator");
         };
     }
 
-    public static String calculatedString(int firstNumber, int secondNumber, char symbol) {
-        return switch (symbol) {
-            case '+' -> firstNumber + " + " + secondNumber;
-            case '-' -> firstNumber + " - " + secondNumber;
-            case '*' -> firstNumber + " * " + secondNumber;
-            default -> throw new RuntimeException("Unknown operator");
-        };
+    public static String questionsCalculated(int firstNumber, int secondNumber, String symbol) {
+        return firstNumber + " " + symbol + " " + secondNumber;
     }
 }
