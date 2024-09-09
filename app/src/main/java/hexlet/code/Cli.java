0dfa65ce6969c32e114c0635fc6945f2abb8b7c6
sergeycherkasovv <iamcherkasov.job@gmail.com>
2024-09-09 22:56:20 +0700

    corrected Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,11 +1,13 @@
 package hexlet.code;
-import java.util.Scanner; //Подключаем Класс Scanner
+
+import java.util.Scanner;
 
 public class Cli {
+
     public static void run() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("May I have your name?");
+        System.out.println(Engine.QUESTION_USER_NAME);
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
     }
